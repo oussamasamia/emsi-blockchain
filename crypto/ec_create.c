caf5dd8afc3c5f13fb8db99cfdb3efa317be0a4a
Oussama Samia <oussamasamia1@gmail.com>
2021-04-17 22:25:49 +0000

    1. EC_KEY creation

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -6,15 +6,15 @@
 
 EC_KEY *ec_create()
 {
-EC_KEY *key = EC_KEY_new();
-EC_GROUP *group;
-if (key == NULL)
-return (NULL);
-group = EC_GROUP_new_by_curve_name(EC_CURVE);
-if (group == NULL)
-return (NULL);
-EC_KEY_set_group(key, group);
-EC_KEY_generate_key(key);
-EC_GROUP_free(group);
-return (key);
+  EC_KEY *key = EC_KEY_new();
+  EC_GROUP *group;
+  if (key == NULL)
+    return (NULL);
+  group = EC_GROUP_new_by_curve_name(EC_CURVE);
+  if (group == NULL)
+    return (NULL);
+  EC_KEY_set_group(key, group);
+  EC_KEY_generate_key(key);
+  EC_GROUP_free(group);
+  return (key);
 }
