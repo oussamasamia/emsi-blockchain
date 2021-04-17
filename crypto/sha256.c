afe9180144f6175aa174c03b0c7ebe0dc5b698ec
Oussama Samia <oussamasamia1@gmail.com>
2021-04-17 17:17:53 +0000

    0.SHA256

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -11,9 +11,9 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-  if (s == NULL || digest == NULL)
-    return (NULL);
-  return (SHA256((const unsigned char *)s, len, digest));
+if (s == NULL || digest == NULL)
+return (NULL);
+return (SHA256((const unsigned char *)s, len, digest));
 }
 
 
