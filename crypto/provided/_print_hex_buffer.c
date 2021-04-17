18aec89a592dea249ee7c940f4ba21446691156b
Oussama Samia <oussamasamia1@gmail.com>
2021-04-17 20:22:36 +0000

    0.SHA256

--- crypto/provided/_print_hex_buffer.c
+++ crypto/provided/_print_hex_buffer.c
@@ -1,3 +1,4 @@
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -15,5 +16,3 @@ void _print_hex_buffer(uint8_t const *buf, size_t len)
   for (i = 0; buf && i < len; i++)
     printf("%02x", buf[i]);
 }
-
-
