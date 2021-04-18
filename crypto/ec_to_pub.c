012cbf72654d664a459fdc32278eda2d92fbfd5e
Oussama Samia <oussamasamia1@gmail.com>
2021-04-18 17:05:23 +0000

    Correcting error : mixing arguments

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -4,11 +4,12 @@
 
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-if (key == NULL || pub == NULL)
-return (NULLÃ);
+const EC_POINT  *publicKey;
+const EC_GROUP  *group;
 
-const EC_POINT = *publicKey;
-const EC_GROUP = *group;
+if (key == NULL || pub == NULL)
+return (NULL);
+
 
 publicKey = EC_KEY_get0_public_key(key);
 group = EC_KEY_get0_group(key);
